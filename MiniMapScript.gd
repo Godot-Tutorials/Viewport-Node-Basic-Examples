735ca46dac27b7e992dba76fee9daae126e9f541
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-08-24 22:17:36 -1000

    Add files via upload

---
+++ MiniMapScript.gd
@@ -0,0 +1,30 @@
+# Basic example of getting a minimap, more code is needed to get it right
+extends Sprite
+
+# Get sizes of bounds and image
+onready var parentViewportWidth: float = 1032 # 1032 is hard coded
+var iconWidth: float = self.get_texture().get_width()
+
+# Movement Speed
+var speed: float = 150.0
+
+# States for Movement
+enum MoveState {MOVE_LEFT, MOVE_RIGHT}
+var state: int = MoveState.MOVE_RIGHT
+
+
+# Activate Movement in Physics Process
+func _physics_process(delta: float) -> void:
+	if self.position.x + iconWidth >= parentViewportWidth:
+		state = MoveState.MOVE_LEFT 
+	if self.position.x <= 0:
+		state = MoveState.MOVE_RIGHT
+	movement(delta)
+
+
+# Hold the moving values
+func movement(deltaValue: float) -> void:
+	if (state == MoveState.MOVE_RIGHT):
+		self.position.x += deltaValue * speed
+	if(state == MoveState.MOVE_LEFT):
+		self.position.x -= deltaValue * speed
